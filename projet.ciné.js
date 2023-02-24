2a88640220b9f38ed903c534899feb33181199de
59013-97-11 <59013-97-11@exchange.ad.afpanet>
2023-02-24 16:41:29 +0100

    fgeg

--- projet.ciné.js
+++ projet.ciné.js
@@ -26,7 +26,7 @@ let films = [
         nom : "Scarface",
         value : false,
         date : "2001",
-        disponibilité : "Non disponible",
+        disponibilité : "Non disponible", 
     },
     {
         id : 4,
@@ -37,26 +37,121 @@ let films = [
     },
 
 ]
+function Ajoutfilms(Titre, DateSortie, Acteurs) {
+    this.Titre = Titre;
+    this.DateSortie = DateSortie;
+    this.Acteurs = Acteurs;
+  }
+//   let NouveauFilm = [];
+  
+  let début = prompt ("Pour voir la liste des films tapez 1\nPour connaitre la liste des films avec leurs date de sortie tapez 2\nPour connaitre les films disponibles à la location tapez 3\nPour connaitre les films non disponibles tapez 4\n5")
 
-films.forEach(film => {
-    console.log(film.nom)
-});
+//   let Nomdufilm = prompt ("Tapez un nom de film à rechercher")
+  
+  switch (début) {
+    case '1':
+        films.forEach(Listedesfilms => {
+            console.log(Listedesfilms.nom)
+        })
+        break
+    case '2':
+        films.forEach(Listedesfilms => {
+            console.log((Listedesfilms.nom),(Listedesfilms.date))
+        })
+        break
+    case '3':
+        films.forEach( film => {
+        if (film.value === true) {
+            console.log(`Film disponibles à la location : ${film.nom}`)
+            
+        }
+        })
+        let choixfilm = prompt(`Si vous voulez louer un film, tapez son nom :`)
+        if (films.nom === choixfilm) {
+            films.value === false
+            console.log(films)
+        }
+        films.forEach(ListedesfilmsDisponible => {
+        console.log(ListedesfilmsDisponible.value===true)
+        })
+        break
+    case '4':
+        films.forEach( film => {
+        if (film.value === false) {
+            console.log(`Film non disponibles à la location : ${film.nom}`)
+        }
+        })
+        break
+    case '5':
+        let Titre = prompt ("Tapez un nom de film à ajouté")
+        let DateSortie = prompt ("Sa date de sortie")
+        let Acteurs = prompt ("Citez le ou les acteurs")
+        let nouveaufilms = new Ajoutfilms(Titre, DateSortie, Acteurs);
+        films.push(nouveaufilms);
+        console.log("Le film a bien été ajouté")
+        console.log(films[films.length-1])
+        break
+  }
+  
+  // function fiilms (nom,value,date,disponibilité)
 
-films.forEach(film => {
-    console.log((film.nom),(film.date))
-});
 
-console.log(films)
-// console.log(films.nom)
-// console.log(films.nom).length
-// console.log(films[".nom"])
-// console.log(films[".nom"].length)
-// console.log(films["nom".length])
-// console.log(films[nom.length])
-// console.log(films[nom].length)
-// console.log(films[nom])
-// console.log(films[(nom)])
-// console.log(films[(nom).length])
-// console.log(films[(nom)].length)
-// console.log(films(nom))
 
+// switch (films) {
+//     case films:
+//         console.log(films)
+//         break
+//     case films.date:
+//         console.log(films.date)
+//         break
+//     default:
+//         break
+// }
+
+// films.forEach( film => {
+//     console.log((film.nom),(film.date))
+// });
+
+// films.forEach(film => {
+//     console.log
+// });
+
+
+
+
+// Apprentissage switch :
+
+// prompt("Combien as tu gagné d'argent ?")
+// let argent = 0;
+// switch (argent) {
+//     case 20 :
+//         console.log(`Félicitation, tu as gagné ${argent}€ !`)
+//         break
+
+//     case 150 :
+//         console.log(`Félicitation, tu as gagné ${argent}€ !`)
+//         break
+
+//     case 250 :
+//         console.log(`Félicitation, tu as gagné ${argent}€ !`)
+//         break
+
+//     case 350 :
+//         console.log(`Félicitation, tu as gagné ${argent}€ !`)
+//         break
+    
+//     case 480 :
+//         console.log(`Félicitation, tu as gagné ${argent}€ !`)
+//         break
+
+//     case 850 :
+//         console.log(`Félicitation, tu as gagné ${argent}€ !`)
+//         break
+// }
+
+
+
+//   films = new film('i', 'i', 'i');
+//   films.push(film1);
+  
+//   films.forEach((element) => console.log(element.film1));
